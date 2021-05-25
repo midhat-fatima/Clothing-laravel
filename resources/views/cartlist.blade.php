6ac3b4482394959a3f5ccde88129b1474d91d529
midhat-fatima <smidhatfatimaa@gmail.com>
2021-05-26 03:46:24 +0500

    cart_updated

---
+++ resources/views/cartlist.blade.php
@@ -0,0 +1,32 @@
+@extends('master')
+@section('content')
+
+<div class="custom-product">
+    <div class="col-sm-10">
+        <div class="treading-wrapper">
+            <h2>Cart List</h2>
+            <div class="">
+                @foreach($products as $item)
+                    <div class="row searching-item cart-list-divider">
+                        <div class="col-sm-3">
+                            <a href="detail/{{$item->id}}">
+                            <img class="treading-img" src="{{$item->gallery}}">
+                            </a>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="">
+                                <h3>{{$item->name}}</h3>
+                                <h5>{{$item->description}}</h5>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                                <a href="removecart/{{$item->cart_id}}" class="btn btn-warning">Remove From Cart</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
